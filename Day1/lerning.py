fc1c6565896e16a01219d743b28573f5a8e68b4d
SaiManikanta <saimanikantam018@gmail.com>
2026-07-08 22:56:14 +0530

    Day1:Lerned python basics

---
+++ Day1/lerning.py
@@ -0,0 +1,25 @@
+    #Variables""        #Datatypes
+name="Sai"              #string
+age=19                  #integer
+height=5.9              #float
+is_student=True         #boolean
+print(name)
+print(age)
+print(height)
+print(is_student)
+
+
+    #checking type
+print(type(name))
+print(type(age))
+
+
+    #Input and Output Statements
+name=input("Enter your name: ")
+print("Hello, " + name + "! Welcome to Python programming." )
+age=int(input("Enter your age: "))
+print("You are " + str(age) + " years old.")
+
+
+""" This is a multi-line comment in Python. It can span multiple lines and is often used for documentation or explanations within the code. Multi-line comments are enclosed within triple quotes (either single or double). """    
+# This is a single-line comment in Python. It starts with the hash symbol (#) and continues until the end of the line. Single-line comments are used to provide brief explanations or notes about specific lines of code.

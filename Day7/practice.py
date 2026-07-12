71c02f29d427180551315ec7c4f04e5a8b1330b7
SaiManikanta <saimanikantam018@gmail.com>
2026-07-12 11:55:41 +0530

    Readme Updated

--- Day7/practice.py
+++ Day7/practice.py
@@ -1,6 +1,7 @@
 file= open("dairy.txt","r")
 
 file.write("I have Leaned Today is\n")
-file.write("-->File Handling")
-file.write("-->Exception Handling.")
+file.write("-->File Handling\n")
+file.write("-->Exception Handling.\n")
+file.write("-->And practicedd")
 file.close()
